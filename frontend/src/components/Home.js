a8a69c23713790ba5254fff83adc5abc9d255345
gustavvopenna <gustavo.pema@gmail.com>
2019-05-16 01:19:17 -0500

    Remove Antd

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,13 +1,11 @@
 import React from 'react'
 import { Button } from 'antd-mobile'
 import Form from './BasicInputExample'
-import BasicInputExampleWrapper from './BasicInputExample'
 
 export default function Home() {
   return (
     <div>
       <Button>Hello</Button>
-      <BasicInputExampleWrapper />
     </div>
   )
 }

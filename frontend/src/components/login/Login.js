3f836ba99b05b34e311be6b2712463cf06bc41e0
gustavvopenna <gustavo.pema@gmail.com>
2019-05-16 13:31:36 -0500

    Fix bug page reload on form submit, add toaster to signup

---
+++ frontend/src/components/login/Login.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import { Button, TextInput, Card } from 'react-materialize'
+import AuthService from '../../services/Auth'
+
+const service = new AuthService()
+
+export default class Login extends Component {
+  render() {
+    return <div />
+  }
+}
--- frontend/src/components/signup/Signup.js
+++ frontend/src/components/signup/Signup.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Button, TextInput, Card } from 'react-materialize'
 import AuthService from '../../services/Auth'
+import toastr from 'toastr'
 
 const service = new AuthService()
 
@@ -21,8 +22,15 @@ export default class Signup extends Component {
   }
 
   handleSubmit = e => {
-    service.signup(this.state.form)
-    console.log(this.state.form)
+    e.preventDefault()
+    service
+      .signup(this.state.form)
+      .then(res => {
+        console.log(res)
+        toastr.success('You successfuly created an account')
+        window.localStorage.setItem('loggedUser', JSON.stringify(res.data))
+      })
+      .catch(err => toastr.error('Something went wrong'))
   }
 
   render() {
@@ -30,14 +38,16 @@ export default class Signup extends Component {
       <div className="container">
         <h1>Sign up</h1>
         <Card>
-          <form>
+          <form onSubmit={this.handleSubmit}>
             <TextInput
               name="name"
+              value={this.state.form.name}
               placeholder="Name"
               onChange={this.handleInput}
             />
             <TextInput
               name="email"
+              value={this.state.form.email}
               email
               validate
               label="Email"
@@ -45,11 +55,16 @@ export default class Signup extends Component {
             />
             <TextInput
               name="password"
+              value={this.state.form.password}
               password
               label="Password"
               onChange={this.handleInput}
             />
-            <Button onClick={this.handleSubmit}>Create an account!</Button>
+            <input
+              type="submit"
+              value="Create an account"
+              className="waves-effect waves-light btn"
+            />
           </form>
         </Card>
       </div>

ee6ab4b353a906cc4300b3caae301ab4e8846cc9
gustavvopenna <gustavo.pema@gmail.com>
2019-05-19 01:27:22 -0500

    Modify styles

--- frontend/src/components/SideNavbar.js
+++ frontend/src/components/SideNavbar.js
@@ -2,11 +2,13 @@ import React, { Component } from 'react'
 import { SideNav, SideNavItem } from 'react-materialize'
 import { Link } from 'react-router-dom'
 import AuthService from '../services/Auth'
-//import history from './history'
 import { withRouter } from 'react-router-dom'
 import toastr from 'toastr'
+import logo from '../images/logo.png'
 
 const service = new AuthService()
+const background =
+  'https://images.template.net/wp-content/uploads/2015/08/Solid-Free-Purple-Background-for-You.png'
 
 class SideNavbar extends Component {
   handleLogout = e => {
@@ -15,7 +17,6 @@ class SideNavbar extends Component {
       .then(res => {
         window.localStorage.clear()
         toastr.success('Logout successful!')
-        //history.push('/login')
         this.props.history.push('/login')
       })
       .catch(err => toastr.error(err))
@@ -23,27 +24,26 @@ class SideNavbar extends Component {
 
   render() {
     return (
-      <div>
+      <div className="sidenav-close">
         <SideNav options={{ closeOnClick: true }}>
           <SideNavItem
             userView
             user={{
-              background: 'https://placeimg.com/640/480/tech',
-              image: 'static/media/react-materialize-logo.824c6ea3.svg',
-              name: 'John Doe'
+              image: logo,
+              background: background,
+              name: 'My Subs'
             }}
           />
           <Link to={'/'}>
             <SideNavItem>Home</SideNavItem>
           </Link>
+          <Link to={'/suscriptions'}>
+            <SideNavItem>Subscriptions</SideNavItem>
+          </Link>
+          <SideNavItem divider />
           <div onClick={this.handleLogout}>
             <SideNavItem>Logout</SideNavItem>
           </div>
-          <SideNavItem divider />
-          <SideNavItem subheader>Subheader</SideNavItem>
-          <SideNavItem waves href="#!third">
-            Third Link With Waves
-          </SideNavItem>
         </SideNav>
       </div>
     )

203456df8333c111ca265788276f4a045e32ab78
gustavvopenna <gustavo.pema@gmail.com>
2019-05-19 00:11:42 -0500

    Add color and images

--- backend/bin/seeds.js
+++ backend/bin/seeds.js
@@ -11,7 +11,10 @@ const typeSubscriptions = [
       oneScreen: { name: '1 pantalla', price: 99 },
       twoScreen: { name: '2 pantallas FHD', price: 129 },
       fourScreen: { name: '4 pantallas 4K', price: 229 }
-    }
+    },
+    color: '#E50914',
+    imageURL_color: 'https://image.flaticon.com/icons/svg/870/870910.svg',
+    imageURL_black: 'https://image.flaticon.com/icons/svg/732/732082.svg'
   },
   {
     name: 'Spotify',
@@ -19,19 +22,30 @@ const typeSubscriptions = [
       student: { name: 'Estudiante', price: 49 },
       premium: { name: 'Premium', price: 99 },
       family: { name: 'Familia', price: 149 }
-    }
+    },
+    color: '#1ED761',
+    imageURL_color: 'https://image.flaticon.com/icons/png/512/226/226773.png',
+    imageURL_black: 'https://image.flaticon.com/icons/svg/152/152756.svg'
   },
   {
     name: 'Amazon Prime',
     version: {
       prime: { name: 'Prime', price: 899 }
-    }
+    },
+    color: '#F9F9F9',
+    imageURL_color:
+      'https://upload.wikimedia.org/wikipedia/commons/2/27/Amazon_Prime_logo.png',
+    imageURL_black:
+      'https://upload.wikimedia.org/wikipedia/commons/2/27/Amazon_Prime_logo.png'
   },
   {
     name: 'HBO',
     version: {
       hboGo: { name: 'HBO Go', price: 99 }
-    }
+    },
+    color: '#000',
+    imageURL_color: 'https://image.flaticon.com/icons/svg/870/870903.svg',
+    imageURL_black: 'https://image.flaticon.com/icons/svg/871/871005.svg'
   },
   {
     name: 'Apple Music',
@@ -39,21 +53,20 @@ const typeSubscriptions = [
       premium: { name: 'premium', price: 99 },
       student: { name: 'Estudiante', price: 49 },
       family: { name: 'Familia', price: 149 }
-    }
+    },
+    color: '#F9F9F9',
+    imageURL_color: 'https://image.flaticon.com/icons/svg/1384/1384061.svg',
+    imageURL_black: 'https://image.flaticon.com/icons/svg/1384/1384029.svg'
   },
   {
     name: 'Dropbox',
     version: {
       plus: { name: 'Plus', price: 199 },
       professional: { name: 'Professional', price: 999 }
-    }
-  },
-  {
-    name: 'Claro Video',
-    version: {
-      standart: { name: 'Estandar', price: 99 },
-      family: { name: 'Familia 4 pantallas', price: 249 }
-    }
+    },
+    color: '#007BE8',
+    imageURL_color: 'https://image.flaticon.com/icons/svg/174/174845.svg',
+    imageURL_black: 'https://image.flaticon.com/icons/svg/25/25250.svg'
   }
 ]
 

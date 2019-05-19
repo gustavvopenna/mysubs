243094ad3b8745b0780891711f501caa10acb70d
gustavvopenna <gustavo.pema@gmail.com>
2019-05-19 00:13:38 -0500

    Add background and image properties

--- frontend/src/components/newSubscription/subscriptionsList.js
+++ frontend/src/components/newSubscription/subscriptionsList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import AuthService from '../../services/Auth'
 import { CardPanel, Row, Col } from 'react-materialize'
+import spotifyImage from '../../images/icons/black/spotify-64.png'
 
 const service = new AuthService()
 
@@ -26,10 +27,16 @@ export default class subscriptionsList extends Component {
         <h1>Subscriptions List</h1>
         {this.state.subscriptions.map(sub => {
           return (
-            <Row key={sub._id}>
+            <Row key={sub._id} style={{ margin: '0px' }}>
               <Col m={6} s={12}>
-                <CardPanel className="teal">
-                  <span className="white-text">{sub.name}</span>
+                <CardPanel
+                  style={{
+                    backgroundColor: sub.color
+                  }}
+                >
+                  <span className="white-text">
+                    <img src={sub.imageURL_black} alt={sub.name} width="45" />
+                  </span>
                 </CardPanel>
               </Col>
             </Row>

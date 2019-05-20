5e1d65d056b485f2aaf8b2b3df60cdda65f0cac7
gustavvopenna <gustavo.pema@gmail.com>
2019-05-20 17:08:22 -0500

    Add owner property

--- backend/models/Subscription.js
+++ backend/models/Subscription.js
@@ -3,34 +3,44 @@ const mongoose = require('mongoose')
 const { Schema } = mongoose
 const { ObjectId } = Schema.Types
 
-const subscriptionSchema = new Schema({
-  subscription: {
-    type: ObjectId,
-    ref: 'TypeSubscription'
+const subscriptionSchema = new Schema(
+  {
+    owner: {
+      type: ObjectId,
+      ref: 'User'
+    },
+    subscription: {
+      type: ObjectId,
+      ref: 'TypeSubscription'
+    },
+    name: {
+      type: String,
+      require: true
+    },
+    planSelected: {
+      type: {},
+      require: true
+    },
+    price: {
+      type: Number,
+      require: true
+    },
+    paymentDate: {
+      type: String,
+      require: true
+    },
+    period: {
+      type: String,
+      enum: ['mensual', 'anual', 'bimestral', 'trimestral', 'semestral'],
+      require: true
+    },
+    paymentMethod: String,
+    labels: []
   },
-  name: {
-    type: String,
-    require: true
-  },
-  planSelected: {
-    type: {},
-    require: true
-  },
-  price: {
-    type: Number,
-    require: true
-  },
-  paymentDate: {
-    type: String,
-    require: true
-  },
-  period: {
-    type: String,
-    enum: ['mensual', 'anual', 'bimestral', 'trimestral', 'semestral'],
-    require: true
-  },
-  paymentMethod: String,
-  labels: []
-})
+  {
+    timestamps: true,
+    versionKey: false
+  }
+)
 
 module.exports = mongoose.model('Subscription', subscriptionSchema)

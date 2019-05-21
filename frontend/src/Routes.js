5a038a7a80d374d87ce75ab27836defef8dcfe7b
gustavvopenna <gustavo.pema@gmail.com>
2019-05-21 07:27:15 -0500

    Modify new Nabvar import

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,14 +3,14 @@ import { Switch, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Signup from './components/signup/Signup'
 import Login from './components/login/Login'
-import SideNavbar from './components/SideNavbar.js'
+import NavComponent from './components/NavComponent.js'
 import Subscriptions from './components/suscriptions/Subscriptions'
 import subscriptionsList from './components/newSubscription/subscriptionsList'
 import subscriptionForm from './components/newSubscription/subscriptionForm'
 
 const Routes = () => (
   <div>
-    <SideNavbar />
+    <NavComponent />
     <Switch>
       <Route exact path="/" component={Home} />
       <Route exact path="/signup" component={Signup} />

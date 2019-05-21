674dee14dbb0742042f4a9959e221d19e011a32d
gustavvopenna <gustavo.pema@gmail.com>
2019-05-21 12:12:30 -0500

    Make border and image to match with subscription type

--- frontend/src/components/suscriptions/CardComponent.js
+++ frontend/src/components/suscriptions/CardComponent.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import logo from '../../images/logo.png'
 
-export default function CardComponent({ name, period, paymentDate }) {
+export default function CardComponent({
+  name,
+  period,
+  paymentDate,
+  subscription
+}) {
   return (
     <div>
-      <ul className="collection">
+      <ul
+        className="collection"
+        style={{ border: `1px solid ${subscription.color}` }}
+      >
         <li className="collection-item avatar">
-          <img src={logo} alt="" className="circle" />
+          <img src={subscription.imageURL_color} alt="" className="circle" />
           <p style={{ textAlign: 'left' }} className="title">
             {name}
           </p>
